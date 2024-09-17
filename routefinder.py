cd81ecf05d4bc9ecce4eec421689c25dd7be0268
ainsleeSmith <apsmith3@dons.usfca.edu>
2024-09-16 19:27:46 -0700

    CONFUSED

--- routefinder.py
+++ routefinder.py
@@ -40,7 +40,36 @@ def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
     closed_list = {}
     search_queue.put(start_state)
     ## you do the rest.
+    if use_closed_list :
+        closed_list[start_state] = True
+    while len(search_queue) > 0 :
+        ## this is a (state, "action") tuple
+        next_state = search_queue.get()
+        # state_count = state_count + 1 ## I added this
+        if goal_test(next_state[0]):
+            print("Goal found")
+            print(next_state)
+            # print("States count: " + str(state_count))
+            ptr = next_state[0]
+            while ptr is not None :
+                ptr = ptr.prev
+                print(ptr)
+            # print("state count: " + str(state_count))
+            return next_state
+        else :
+            successors = next_state.mars_graph.get_edges(next_state.location)
+            for s in successors:
+                search_queue.put(s)
 
+            # successors = next_state[0].successors(next_state.)
+            # state_count = state_count + len(successors)  ## I added
+            # print("state count: " + str(state_count))
+            # if use_closed_list :
+            #     successors = [item for item in successors
+            #                         if item[0] not in closed_list]
+            #     for s in successors :
+            #         closed_list[s[0]] = True
+            # search_queue.extend(successors)
 
 ## default heuristic - we can use this to implement uniform cost search
 def h1(state) :
@@ -49,6 +78,7 @@ def h1(state) :
 ## you do this - return the straight-line distance between the state and (1,1)
 def sld(state) :
     # sqt(a^2 + b2)
+    # print("location: " + state.location)
     info = state.location.split(',')
     x = info[0]
     y = info[1]
