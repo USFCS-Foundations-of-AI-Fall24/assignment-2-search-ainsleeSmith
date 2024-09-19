3612aaf8a7cf8fc4198c858202259544e4a16bdb
ainsleeSmith <apsmith3@dons.usfca.edu>
2024-09-18 18:18:54 -0700

    A* cleaned and tested

--- routefinder.py
+++ routefinder.py
@@ -44,26 +44,17 @@ def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
     if use_closed_list :
         closed_list[start_state.location] = True
     while not search_queue.empty() :
-        print ("it not empty")
         ## this is a (state, "action") tuple
         next_state = search_queue.get()
-        # print("searching location : ")
-        # print(next_state.location)
         # state_count = state_count + 1 ## I added this
         if goal_test(next_state):
             print("Goal found")
-            print(next_state)
-            # print("States count: " + str(state_count))
             ptr = next_state
             while ptr is not None :
                 ptr = ptr[1].prev_state
-                # print(ptr)
-            # print("state count: " + str(state_count))
             return next_state
         else :
-            print("this is not the goal")
             edges = next_state[1].mars_graph.get_edges(next_state[1].location)
-            # print("edges: " + str(edges))
             successors = []
             for e in edges:
                 e = str(e)
@@ -76,8 +67,6 @@ def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
                     new = map_state(location=s)
                     new.mars_graph = next_state[1].mars_graph
                     new.g = next_state[1].g + 1 # since no weight provided for marsmap, assuming 1
-                    # print("location: ")
-                    # print(s)
                     new.h = heuristic_fn(new)
                     new.f = new.g + new.h
                     search_queue.put((new.f,new))
@@ -125,6 +114,3 @@ if __name__=="__main__" :
     s1.h = sld(s1)
     result = a_star(s1, sld, goal_complete)
     print(result)
-    # read_mars_graph('MarsMap')
-    # print("Map: ")
-    # print(read_mars_graph('MarsMap'))
